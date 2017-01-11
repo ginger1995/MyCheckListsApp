625f54a15858a7e21dda03b3f6d824a11306614f
ginger1995 <jbw7@qq.com>
2017-01-11 09:57:06 +0800

    .

---
+++ CheckLists/AddItemViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddItemViewController.swift
+//  CheckLists
+//
+//  Created by ginger on 2017/1/10.
+//  Copyright © 2017年 ginger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- CheckLists/ChecklistViewController.swift
+++ CheckLists/ChecklistViewController.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
     
+    var items :[ChecklistItem]
+    
+    required init?(coder aDecoder: NSCoder) {
+        items = [ChecklistItem]()
+        
+        let row0item = ChecklistItem()
+        row0item.text = "Walk the dog"
+        row0item.checked = false
+        items.append(row0item)
+        
+        let row1item = ChecklistItem()
+        row1item.text = "Brush my teeth"
+        row1item.checked = false
+        items.append(row1item)
+        
+        let row2item = ChecklistItem()
+        row2item.text = "Learn iOS development"
+        row2item.checked = false
+        items.append(row2item)
+        
+        let row3item = ChecklistItem()
+        row3item.text = "Soccer practice"
+        row3item.checked = false
+        items.append(row3item)
+        
+        let row4item = ChecklistItem()
+        row4item.text = "Eat ice cream"
+        row4item.checked = false
+        items.append(row4item)
+        
+        super.init(coder: aDecoder)
+    }
+    //点击navigation controller上的“加号”时触发
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        item.checked = true
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,79 +65,51 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //覆盖Datasource protocol中的方法：方法返回tableview的总行数
+    //覆盖UItableViewDatasource protocol中的方法：方法返回tableview的总行数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return items.count
     }
-    //覆盖Datasource protocol中的方法：方法为索引指定的行返回一个cell
+    //覆盖UItableViewDatasource protocol中的方法：方法为索引指定的行返回一个cell，通过该方法为所有的行添加cell（数据）
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //gets a copy of the prototype cell – either a new one or a recycled one – and puts it into the local constant
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
-        let label = cell.viewWithTag(1000) as! UILabel
-        
-        if indexPath.row % 5 == 0 { label.text = "Walk the dog"
-        } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
-        } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
-        } else if indexPath.row % 5 == 3 {
-            label.text = "Soccer practice"
-        } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream"
-        }
+        let item = items[indexPath.row]
+        configureText(for:cell, with:item)
+        configureCheckmark(for:cell, with:item)
         return cell
     }
     //覆盖UItableviewdelegate协议中的方法，gets called whenever the user taps on a cell.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
+            let item = items[indexPath.row]
+            item.toggleChecked()
+            configureCheckmark(for:cell, with:item)
         }
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //覆盖UItableViewDatasource protocol中的方法：Asks the data source to commit the insertion or deletion of a specified row in the receiver.(右划item删除项目)
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        //1.remove the item from the data model
+        items.remove(at: indexPath.row)
+        //2.delete the corresponding row from the table view
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
+    func configureCheckmark(for cell:UITableViewCell, with item:ChecklistItem) {
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+    
+    func configureText(for cell:UITableViewCell, with item:ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
+    }
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
